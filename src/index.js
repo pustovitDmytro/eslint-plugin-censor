1759243964f01f862f27efd85a582bc823676eb0
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-05-03 20:28:29 +0300

    New: adds recommended config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 module.exports = {
     rules : {
         'no-swear' : require('./rules/no-swear')
+    },
+    configs : {
+        recommended : {
+            rules : {
+                'censor/no-swear' : 2
+            }
+        }
     }
 };
